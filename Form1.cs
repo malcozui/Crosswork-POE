49e5a5a1106ebf39fb1c6a949b7dc8b1dbd9c804
malcozui <malcozui@gmail.com>
2022-06-02 17:27:11 +0200

    removed comment

--- Form1.cs
+++ Form1.cs
@@ -12,7 +12,6 @@ namespace CrosswordPOE_Team
 {
     public partial class CrosswordForm : Form
     {
-        //comment
         public CrosswordForm()
         {
             InitializeComponent();
